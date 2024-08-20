d47dedef7d2a383065433821a8f64b8b3b29f996
Fernando Perez <refzac@gmail.com>
2024-08-20 12:37:13 -0600

    BigIntegerFiel Telephone

--- clients/models.py
+++ clients/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
-    telephone = models.IntegerField()
+    telephone = models.BigIntegerField()
     create_time=models.DateTimeField(auto_now_add=True)
